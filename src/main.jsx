9778b98ff9024c57295a10a0e71f3b9ec99ea770
baileylloyd211-sketch <baileylloyd211@gmail.com>
2026-03-10 00:29:40 -0700

    Update import path for Ledger component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './hooks/useAuth'
-import Ledger from './components/Ledger.jsx'
+import Ledger from './components/Ledger'
 import AuthPage from './components/AuthPage.jsx'
 import './index.css'
 
