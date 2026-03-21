6a4bd8d091751bae4d5622dd368a8907a928833c
baileylloyd211-sketch <baileylloyd211@gmail.com>
2026-03-21 01:46:15 -0700

    Update main.jsx
    no I'mport auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider, useAuth } from './hooks/useAuth'
 import Ledger from './components/Ledger'
 import AuthPage from './components/AuthPage.jsx'
 import './index.css'
